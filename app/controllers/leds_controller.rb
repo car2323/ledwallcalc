075512d3c7adf79e07944b010a6d1813cd32fac9
Carlos Cardo <ccardo29@hotmail.com>
2016-06-03 22:07:16 -0400

    show to show led details and delete or update

--- app/controllers/leds_controller.rb
+++ app/controllers/leds_controller.rb
@@ -1,8 +1,23 @@
 class LedsController < ApplicationController
 	before_action :authenticate_user!
 	def new
-		@my_user = current_user.id
 		@my_led = Led.new
 		render "new"
+	end
+	def create		
+		puts "paso por aqui %%%%%%%%%%%%%%%%%%%%%%%%"
+		@my_ledpanel=Led.new(
+			:user_id => current_user.id,
+			:model => params[:led][:model],
+			:brand => params[:led][:brand],
+			:panelsize_w => params[:led][:panelsize_w],
+            :panelsize_h => params[:led][:panelsize_h],
+			:panelweight => params[:led][:panelweight],
+			:pixelmatrix_w => params[:led][:pixelmatrix_w],
+			:pixelmatrix_h => params[:led][:pixelmatrix_h],
+			:poweramp110 => params[:led][:poweramp110],
+			:poweramp220 => params[:led][:poweramp220])
+		@my_ledpanel.save
+		redirect_to "/informations"
 	end
 end
