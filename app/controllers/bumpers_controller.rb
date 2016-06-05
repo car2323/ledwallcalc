1d59e037c12b3894fcbe953abbb0da3e0e92e2ae
Carlos Cardo <ccardo29@hotmail.com>
2016-06-05 16:33:41 -0400

    add new bumper to oneledpanel

--- app/controllers/bumpers_controller.rb
+++ app/controllers/bumpers_controller.rb
@@ -10,19 +10,16 @@ class BumpersController < ApplicationController
        
     end
 		
-	def create		
-	@my_ledpanel=Led.new(
-			:user_id => current_user.id,
-			:model => params[:led][:model],
-			:brand => params[:led][:brand],
-			:panelsize_w => params[:led][:panelsize_w],
-            :panelsize_h => params[:led][:panelsize_h],
-			:panelweight => params[:led][:panelweight],
-			:pixelmatrix_w => params[:led][:pixelmatrix_w],
-			:pixelmatrix_h => params[:led][:pixelmatrix_h],
-			:poweramp110 => params[:led][:poweramp110],
-			:poweramp220 => params[:led][:poweramp220])
-		@my_ledpanel.save
+	def create	
+		    puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+	        puts params[:bumper][:led_id]
+	        puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+	@one_bumper=Bumper.new(
+    
+			:description => params[:bumper][:description],
+			:weight => params[:bumper][:weight],
+			:height => params[:bumper][:height])
+		@one_bumper.save
 		redirect_to "/informations"
 	end
     def destroy
