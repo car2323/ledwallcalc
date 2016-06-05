1cff1cd88d9fb4747dc161e8762ac09a40189073
Carlos Cardo <ccardo29@hotmail.com>
2016-06-05 00:42:14 -0400

    creando add update and modify bumpers

--- app/controllers/bumpers_controller.rb
+++ app/controllers/bumpers_controller.rb
@@ -14,38 +14,12 @@ class BumpersController < ApplicationController
 	
 	end
     def destroy
-        @one_bumper = Bumper.find_by(id: params[:id])
-        if @one_bumper.nil?
-            render "informations"
-            return
-        end
-		@one_bumper.destroy
-           redirect_to "/informations"
-               
+    
 	end
 	def edit
-     @one_ledpanel = Led.find_by(id: params[:id])
+     @one_bumper = Bumper.find_by(id: params[:id])
 	end
     def  update
-	  @one_ledpanel = Led.find_by(id: params[:id])
-      if params[:led]!= nil
-	      if @one_ledpanel.update(
-				:model => params[:led][:model],
-				:brand => params[:led][:brand],
-				:panelsize_w => params[:led][:panelsize_w],
-	            :panelsize_h => params[:led][:panelsize_h],
-				:panelweight => params[:led][:panelweight],
-				:pixelmatrix_w => params[:led][:pixelmatrix_w],
-				:pixelmatrix_h => params[:led][:pixelmatrix_h],
-				:poweramp110 => params[:led][:poweramp110],
-				:poweramp220 => params[:led][:poweramp220])
-	            redirect_to "/informations"
-	      else
-            render "update"
-          end
-      else
-       	    edit()
-      end
-		
+	  
 	end
 end
