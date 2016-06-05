010b36b4709576f3331a1a6271d7b27a90ac3f40
Carlos Cardo <ccardo29@hotmail.com>
2016-06-05 00:33:13 -0400

    starting with bumper controller

--- app/controllers/leds_controller.rb
+++ app/controllers/leds_controller.rb
@@ -8,9 +8,7 @@ class LedsController < ApplicationController
 	def show 
        @one_ledpanel = Led.find_by(id: params[:id])
        @bumpers = @one_ledpanel.bumper(led_id: params[:id])
-       puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-       puts @bumpers
-       puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+       
     end
 		
 	def create		
