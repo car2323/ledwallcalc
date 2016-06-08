935afd56964683f3ec7b18717ad3264820211f0c
Carlos Cardo <ccardo29@hotmail.com>
2016-06-07 21:37:41 -0400

    add modal with div inside

---
+++ app/assets/javascripts/leds.js
@@ -0,0 +1,20 @@
+$(document).on("page:load ready", function () {
+  $(".modal-body").empty();
+  $(".modal_title").empty();
+  //console.log("llego a agregar equipment"); 
+
+  $(".js-calculate").on("click", function (event) {
+      event.preventDefault();
+      event.preventDefault();
+      console.log ("holaaaaaaaaaaaaaaaaaa");
+      // var equipment_id_btn = $(".js-updateequi");
+      // var equipment_id = equipment_id_btn.data("equip");
+
+      // display_create_rental();
+      // $(".create_rental").on("click",function(){
+      // var post_data = {
+      // name: $("#name_rental").val(),
+      // date: $("#date_rental").val(),
+      // total_price: $("#total_price_rental").val(),
+  }
+}
