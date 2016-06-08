1c65d2ee1a5f555c2fe742c1bedb46c4a174f3db
Carlos Cardo <ccardo29@hotmail.com>
2016-06-07 21:59:00 -0400

    modal show ready

--- app/assets/javascripts/leds.js
+++ app/assets/javascripts/leds.js
@@ -1,11 +1,11 @@
 $(document).on("page:load ready", function () {
-  
+     
    console.log ("holaaaaaaaaaaaaaaaaaa");
 
   $(".js-calculate").on("click", function (event) {
-      
+      $(".modal").modal("show");
       console.log ("holaaaaaaaaaaaaaaaaaa");
-      $(".gridSystemModal").show();
+   
      
   });
 });
