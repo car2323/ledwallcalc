a3d17f43dc49a2e9b1bd394362f1a0c4573b5278
Carlos Cardo <ccardo29@hotmail.com>
2016-06-07 21:45:58 -0400

    testing modal grip

--- app/assets/javascripts/leds.js
+++ app/assets/javascripts/leds.js
@@ -1,20 +1,11 @@
 $(document).on("page:load ready", function () {
-  $(".modal-body").empty();
-  $(".modal_title").empty();
-  //console.log("llego a agregar equipment"); 
+  
+   console.log ("holaaaaaaaaaaaaaaaaaa");
 
   $(".js-calculate").on("click", function (event) {
-      event.preventDefault();
-      event.preventDefault();
+      
       console.log ("holaaaaaaaaaaaaaaaaaa");
-      // var equipment_id_btn = $(".js-updateequi");
-      // var equipment_id = equipment_id_btn.data("equip");
-
-      // display_create_rental();
-      // $(".create_rental").on("click",function(){
-      // var post_data = {
-      // name: $("#name_rental").val(),
-      // date: $("#date_rental").val(),
-      // total_price: $("#total_price_rental").val(),
-  }
-}
+      $(".gridSystemModal").show();
+     
+  });
+});
