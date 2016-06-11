d5fbdf6fa9c854143bbbb526830821d1def67a9b
Carlos Cardo <ccardo29@hotmail.com>
2016-06-11 19:55:41 -0400

    testing modal don't show info

--- app/assets/javascripts/leds.js
+++ app/assets/javascripts/leds.js
@@ -1,22 +1,8 @@
 $(document).on("page:load ready", function () {
-   $(".modal-body").empty();
-   $(".modal_title").empty(); 
+   
   $(".js-calculate").on("click", function (event) {
       $(".modal").modal("show");
         
       
-      var string_url= "/equipments/"+ equipment_id +"/rentals/"+ rental_id;
-       $.ajax({     
-              url: string_url,
-              success: function (rental){
-                 display_rental(rental, string_url);
-                 console.log("Success");
-              },
-              error:function (rental) {
-                  console.log("It failed. :( ");
-                  console.log(theError.responseJSON);
-              }
-            }); 
-     
-  });
+    
 });
