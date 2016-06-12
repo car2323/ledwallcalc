7d9bb34bf35edb251a0fdb2e39a1f546100f3610
Carlos Cardo <ccardo29@hotmail.com>
2016-06-11 21:21:11 -0400

    fix data hook js

--- app/assets/javascripts/leds.js
+++ app/assets/javascripts/leds.js
@@ -7,7 +7,7 @@ $(document).on("page:load ready", function () {
        $(".js-ledlist").on("click", function (event) {
              $(".col-sm-9").empty();
              console.log("adentro del click");
-             var led_id_list = $(".js-ledlist");
+             var led_id_list = $(event.currentTarget);
              var led_id = led_id_list.data("lediid");
              $(".col-sm-9").append(led_id);
 
