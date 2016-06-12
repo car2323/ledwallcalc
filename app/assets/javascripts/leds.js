ec9b6820be5d4d743d2551120dea0926fa2bbefc
Carlos Cardo <ccardo29@hotmail.com>
2016-06-11 21:14:16 -0400

    problems with boot strap and refresh modal

--- app/assets/javascripts/leds.js
+++ app/assets/javascripts/leds.js
@@ -1,6 +1,20 @@
 $(document).on("page:load ready", function () {
      $(".js-calculate").on("click", function (event) {
-      $(".modal").modal("show");
+       event.preventDefault();
+       $(".col-sm-9").empty();
+       $(".modal").modal("show");
+       console.log("afuera del click");
+       $(".js-ledlist").on("click", function (event) {
+             $(".col-sm-9").empty();
+             console.log("adentro del click");
+             var led_id_list = $(".js-ledlist");
+             var led_id = led_id_list.data("lediid");
+             $(".col-sm-9").append(led_id);
+
+
+
+       });
+
        
   });
 });
