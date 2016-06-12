c8105fdaa92a62761424940e68d6996acd4b5158
Carlos Cardo <ccardo29@hotmail.com>
2016-06-11 22:08:17 -0400

    modal row new to add values for screen

--- app/assets/javascripts/leds.js
+++ app/assets/javascripts/leds.js
@@ -1,22 +1,27 @@
 $(document).on("page:load ready", function () {
      $(".js-calculate").on("click", function (event) {
        event.preventDefault();
-       $(".col-sm-9").empty();
+       $(".level1led").empty();
        $(".modal").modal("show");
-       console.log("afuera del click");
+    
        $(".js-ledlist").on("click", function (event) {
-             $(".col-sm-9").empty();
-             console.log("adentro del click");
+             $(".level1led").empty();
+             
              var led_model_list = $(event.currentTarget);
              var led_model = led_model_list.data("ledmodel");
-             $(".col-sm-9").append(led_model);
+             $(".level1led").append(led_model);
              $(".js-screenlist").on("click", function (event) {
-                 console.log("adentro del 2click");
+                 $(".level2screen").empty();
                  var screen_width_list = $(event.currentTarget);
                  var screen_width = screen_width_list.data("screenwidth");
-                 console.log(screen_width);
-                 $("col-sm-9").append(screen_width);
 
+                 var screen_height_list = $(event.currentTarget);
+                 var screen_height = screen_height_list.data("screenheight");
+                  console.log(screen_height);
+                 $(".level2screen").append("<br>");
+                 $(".level2screen").append(screen_width);
+                 $(".level2screen").append(" x ");
+                 $(".level2screen").append(screen_height);
 
              });
 
