19936d211e8b0979381291983a780a61c661e75f
Carlos Cardo <ccardo29@hotmail.com>
2016-06-11 21:48:48 -0400

    problems with data hook screen testing

--- app/assets/javascripts/leds.js
+++ app/assets/javascripts/leds.js
@@ -7,11 +7,18 @@ $(document).on("page:load ready", function () {
        $(".js-ledlist").on("click", function (event) {
              $(".col-sm-9").empty();
              console.log("adentro del click");
-             var led_id_list = $(event.currentTarget);
-             var led_id = led_id_list.data("lediid");
-             $(".col-sm-9").append(led_id);
+             var led_model_list = $(event.currentTarget);
+             var led_model = led_model_list.data("ledmodel");
+             $(".col-sm-9").append(led_model);
+             $(".js-screenlist").on("click", function (event) {
+                 console.log("adentro del 2click");
+                 var screen_width_list = $(event.currentTarget);
+                 var screen_width = screen_width_list.data("screenwidth");
+                 console.log(screen_width);
+                 $("col-sm-9").append(screen_width);
 
 
+             });
 
        });
 
