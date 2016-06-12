832e2cacd81368bef9b1ed84167800863ae87bb2
Carlos Cardo <ccardo29@hotmail.com>
2016-06-12 19:14:03 -0400

    working good validation calculate button

--- app/assets/javascripts/validateselected.js
+++ app/assets/javascripts/validateselected.js
@@ -1,8 +1,9 @@
 function select_validations(){
    var vali_var = true;
-   if(($(".level2screen").val()==="")|| ($(".level1led").val()==="")){
+   console.log("llego a la funcion de validacion");
+
+   if(($(".level2screen").text()==="")|| ($(".level1led").text()==="")){
       vali_var = false;
    }
-
   return vali_var
 }
