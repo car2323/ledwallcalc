6dfbc3c08e0747dd9ec370ff5e3e4e3e1f2cf81d
Carlos Cardo <ccardo29@hotmail.com>
2016-06-18 16:53:28 -0400

    modal data to convert units

--- app/assets/javascripts/converter.js
+++ app/assets/javascripts/converter.js
@@ -1,6 +1,11 @@
 $(document).on("page:load ready", function () {
      $(".js-converter").on("click", function (event) {
 
+     $(".modal-content").append("Probando modal")
+     $(".modal-content").append("<input class='form-control'></input>")
+     $(".modal-content").append("Probando modal")
+     $(".modal-content").append("Probando modal")
+     $(".modal-content").append("<button class='btn btn-primary'></button>")
      $(".modal").modal("show");
 
      });
