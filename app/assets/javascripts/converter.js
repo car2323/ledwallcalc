ae2a6167fb4d69723ce49f7f615acf928f46dd0a
Carlos Cardo <ccardo29@hotmail.com>
2016-06-18 17:07:47 -0400

    ready for show, need calculations yet
    need validation for input

--- app/assets/javascripts/converter.js
+++ app/assets/javascripts/converter.js
@@ -1,11 +1,10 @@
 $(document).on("page:load ready", function () {
      $(".js-converter").on("click", function (event) {
 
-     $(".modal-title").append("Converter Units  ");
-     $(".modal-content").append("<input class='form-control'></input>");
-     $(".modal-content").append("Probando modal");
-     $(".modal-content").append("Probando modal");
-     
+     $(".modal-title").append("<h3 class='colororange'>Converter Units</h3>");
+     $(".modal-body").append("<input class='form-control' placeholder='feet'></input>");
+     $(".modal-body").append("<h3>TO:</h3>");
+     $(".modal-body").append("<input class='form-control' placeholder='inches'></input>");
      $(".modal").modal("show");
 
      });
