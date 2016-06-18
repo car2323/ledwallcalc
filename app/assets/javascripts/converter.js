38943b7258f03097bfb6bc1fbd5ea8389f80d587
Carlos Cardo <ccardo29@hotmail.com>
2016-06-18 17:25:48 -0400

    working good converter

--- app/assets/javascripts/converter.js
+++ app/assets/javascripts/converter.js
@@ -1,15 +1,17 @@
 $(document).on("page:load ready", function () {
      $(".js-converter").on("click", function (event) {
-
-     $(".modal-title").append("<h3 class='colororange'>Converter Units</h3>");
-     $(".modal-body").append("<input onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode === 44 || event.charCode === 46' class='form-control' placeholder='feet'></input>");
-     $(".modal-body").append("<h3>TO:</h3>");
-     $(".modal-body").append("<input disabled='true' onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode === 44 || event.charCode === 46' class='form-control' placeholder='inches'></input>");
-     $(".modal").modal("show");
-
+	     $(".modal-title").empty();
+	     $(".modal-body").empty();
+	     $(".modal-title").append("<h3 class='colororange'>Converter Units</h3>");
+	     $(".modal-body").append("<input onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode === 44 || event.charCode === 46' class='form-control js-feetnumber' placeholder='feet'></input>");
+	     $(".modal-body").append("<h3>TO:</h3>");
+	     $(".modal-body").append("<input disabled='true' onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode === 44 || event.charCode === 46' class='form-control js-inchesnumber' placeholder='inches'></input>");
+	     $(".modal").modal("show");
+	     $(".js-convert").on("click",function (event){
+             var feet_number = $(".js-feetnumber").val();
+             var inches_number = (feet_number * 12);
+             $(".js-inchesnumber").val(inches_number);
+         });
      });
-
-
-
 });
   
