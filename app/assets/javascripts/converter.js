c0b3fd9e63ef2ad64ee47b7a190d346b3c12d0f3
Carlos Cardo <ccardo29@hotmail.com>
2016-06-18 17:13:16 -0400

    validation ready

--- app/assets/javascripts/converter.js
+++ app/assets/javascripts/converter.js
@@ -2,9 +2,9 @@ $(document).on("page:load ready", function () {
      $(".js-converter").on("click", function (event) {
 
      $(".modal-title").append("<h3 class='colororange'>Converter Units</h3>");
-     $(".modal-body").append("<input class='form-control' placeholder='feet'></input>");
+     $(".modal-body").append("<input onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode === 44 || event.charCode === 46' class='form-control' placeholder='feet'></input>");
      $(".modal-body").append("<h3>TO:</h3>");
-     $(".modal-body").append("<input class='form-control' placeholder='inches'></input>");
+     $(".modal-body").append("<input disabled='true' onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode === 44 || event.charCode === 46' class='form-control' placeholder='inches'></input>");
      $(".modal").modal("show");
 
      });
