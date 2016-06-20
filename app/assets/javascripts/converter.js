22a924346ff24a9c44ea87e97057d2cf25ed59b5
Carlos Cardo <ccardo29@hotmail.com>
2016-06-20 19:49:48 -0400

    problems with signed value to convert

--- app/assets/javascripts/converter.js
+++ app/assets/javascripts/converter.js
@@ -1,6 +1,6 @@
 $(document).on("page:load ready", function () {
 	
-     $(".js-converter").on("click", function (event) {
+    $(".js-converterpanelsizew").on("click", function (event) {
      
 	     $(".modal-title").empty();
 	     $(".modal-body").empty();
@@ -13,8 +13,43 @@ $(document).on("page:load ready", function () {
              var feet_number = $(".js-feetnumber").val();
              var inches_number = (feet_number * 12).toFixed(2);
              $(".js-inchesnumber").val(inches_number);
+             $(".js-apply").on("click",function(){
+             	$(".js-panelsizew").val(inches_number);
+             	 $(".modal").modal("hide");
+             	  $(".modal-title").empty();
+	              $(".modal-body").empty();
+             }) 
          });
      });
+
+	 $(".js-converterpanelsizeh").on("click", function (event) {
+	     
+		     $(".modal-title").empty();
+		     $(".modal-body").empty();
+		     $(".modal-title").append("<h3 class='colororange'>Converter Units</h3>");
+		     $(".modal-body").append("<input onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode === 44 || event.charCode === 46' class='form-control js-feetnumber' placeholder='feet'></input>");
+		     $(".modal-body").append("<h3>TO:</h3>");
+		     $(".modal-body").append("<input disabled='true' onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode === 44 || event.charCode === 46' class='form-control js-inchesnumber' placeholder='inches'></input>");
+		     $(".modal").modal("show");
+		     $(".js-convert").on("click",function (event){
+	             var feet_number = $(".js-feetnumber").val();
+	             var inches_number = (feet_number * 12).toFixed(2);
+	             $(".js-inchesnumber").val(inches_number);
+	             $(".js-apply").on("click",function(){
+	             	$(".js-panelsizeh").val(inches_number);
+	             	 $(".modal").modal("hide");
+	             	  $(".modal-title").empty();
+		              $(".modal-body").empty();
+	             }) 
+	         });
+	     });
+
+
+
+
+
+
+
 
      $(".js-converter2").on("click", function (event) {
      	
