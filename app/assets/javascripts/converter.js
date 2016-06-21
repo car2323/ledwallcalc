6ff6573deb144c15beb0171450ea5d2995867be1
Carlos Cardo <ccardo29@hotmail.com>
2016-06-20 20:31:26 -0400

    working good converter panel signed

--- app/assets/javascripts/converter.js
+++ app/assets/javascripts/converter.js
@@ -4,42 +4,51 @@ $(document).on("page:load ready", function () {
      
 	     $(".modal-title").empty();
 	     $(".modal-body").empty();
+	     $(".modal-footer").empty();
 	     $(".modal-title").append("<h3 class='colororange'>Converter Units</h3>");
 	     $(".modal-body").append("<input onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode === 44 || event.charCode === 46' class='form-control js-feetnumber' placeholder='feet'></input>");
 	     $(".modal-body").append("<h3>TO:</h3>");
 	     $(".modal-body").append("<input disabled='true' onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode === 44 || event.charCode === 46' class='form-control js-inchesnumber' placeholder='inches'></input>");
+	     $(".modal-footer").append("<button type='button' class='btn btn-primary js-convert'>convert</button>");
+	     $(".modal-footer").append("<button type='button' class='btn btn-warning js-apply1'>Apply</button>");
 	     $(".modal").modal("show");
 	     $(".js-convert").on("click",function (event){
              var feet_number = $(".js-feetnumber").val();
              var inches_number = (feet_number * 12).toFixed(2);
              $(".js-inchesnumber").val(inches_number);
-             $(".js-apply").on("click",function(){
+             $(".js-apply1").on("click",function(){
              	$(".js-panelsizew").val(inches_number);
              	 $(".modal").modal("hide");
              	  $(".modal-title").empty();
 	              $(".modal-body").empty();
+	              $(".modal-footer").empty();
              }) 
          });
      });
 
-	 $(".js-converterpanelsizeh").on("click", function (event) {
-	     
+	 $(".js-convertpanelsizeh").on("click", function (event) {
+	         console.log("entro por el segundo");
 		     $(".modal-title").empty();
 		     $(".modal-body").empty();
+		     $(".modal-footer").empty();
 		     $(".modal-title").append("<h3 class='colororange'>Converter Units</h3>");
 		     $(".modal-body").append("<input onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode === 44 || event.charCode === 46' class='form-control js-feetnumber' placeholder='feet'></input>");
 		     $(".modal-body").append("<h3>TO:</h3>");
 		     $(".modal-body").append("<input disabled='true' onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode === 44 || event.charCode === 46' class='form-control js-inchesnumber' placeholder='inches'></input>");
+             $(".modal-footer").append("<button type='button' class='btn btn-primary js-convert'>convert</button>");
+		     $(".modal-footer").append("<button type='button' class='btn btn-warning js-apply2'>Apply</button>");
 		     $(".modal").modal("show");
 		     $(".js-convert").on("click",function (event){
 	             var feet_number = $(".js-feetnumber").val();
 	             var inches_number = (feet_number * 12).toFixed(2);
 	             $(".js-inchesnumber").val(inches_number);
-	             $(".js-apply").on("click",function(){
-	             	$(".js-panelsizeh").val(inches_number);
+	             $(".js-apply2").on("click", function (){
+	             	console.log("adentro del segundo apply");
+	             	$(".js-ledpanelsizeh").val(inches_number);
 	             	 $(".modal").modal("hide");
 	             	  $(".modal-title").empty();
 		              $(".modal-body").empty();
+		              $(".modal-footer").empty();
 	             }) 
 	         });
 	     });
@@ -51,7 +60,7 @@ $(document).on("page:load ready", function () {
 
 
 
-     $(".js-converter2").on("click", function (event) {
+     $(".js-converterpanelw2").on("click", function (event) {
      	
 	     $(".modal-title").empty();
 	     $(".modal-body").empty();
