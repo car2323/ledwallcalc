9f9ab9b5114b5772929e4e3ab28d09c44571357e
Carlos Cardo <ccardo29@hotmail.com>
2016-06-25 20:56:34 -0400

    problems with pdf var

--- app/assets/javascripts/createreport.js
+++ app/assets/javascripts/createreport.js
@@ -2,8 +2,8 @@ $(document).on("page:load ready", function () {
 
      $(".js-createpdf").on("click",function (event){
        console.log($(".modelpanelpdf").text());
-       js-panels_w
-       js-panels_h
+       
+       
        create_PDFequip();
       
    });
@@ -26,12 +26,13 @@ function create_PDFequip(){
   doc.text(10,90, "Resolution LED wall: ");
   doc.text(10,100, "Aspect Ratio:");
   doc.text(10,110, "Actual Width size: 59.07 inch or 4.92 feet diff. -12.93 inch:");
-   doc.text(10,120,"Actual Heigth size: 78.76 inch or 6.56 feet diff. -17.24 inch");
+  doc.text(10,120,"Actual Heigth size: 78.76 inch or 6.56 feet diff. -17.24 inch");
   doc.text(10,130, "Total power consuming 110V: 15 amp");
   doc.text(10,140, "Total feed 20 amp in 110V: 2");
-  doc.text(10,150, "Current Difference against original price  plus cost of all maintenance: ");
-  doc.text(10,160, "Price after depreciation: ");
- 
+  doc.text(10,150, "Total feed 20 amp in 110V: 2");
+  doc.text(10,160, "Total power consuming 220V: 7 amp");
+  doc.text(10,170, "");
+  doc.text(10,180, "");
   doc.text(10,190, "REPORT DATE: ");
   
 
@@ -39,37 +40,39 @@ function create_PDFequip(){
   doc.setFontType("bold");
   doc.setTextColor(150);
     doc.text(55,50, $(".modelpanelpdf").text());
-	doc.text(55,60, );
-    doc.text(55,70, one_equipment.model);
+    doc.text(55,60, $(".js-panels_w").text());
+	  doc.text(55,60, $(".js-panels_h").text());
+ //    doc.text(55,70, one_equipment.model);
 
-	doc.text(55,80, one_equipment.serial);
-	doc.text(55,90, one_equipment.brand);
-	var price=one_equipment.original_price.toString();
-	doc.text(55,100, one_equipment.purchased_date);
+	// doc.text(55,80, one_equipment.serial);
+	// doc.text(55,90, one_equipment.brand);
+	// var price=one_equipment.original_price.toString();
+	// doc.text(55,100, one_equipment.purchased_date);
 
-    doc.text(175,140, $(".js-pricelessrental").text() +"$");
-    doc.text(175,150, $(".js-priceplusmainte").text()+"$");
-    doc.text(175,160, $(".js-depretaicioninfo").text()+"$");
+ //    doc.text(175,140, $(".js-pricelessrental").text() +"$");
+ //    doc.text(175,150, $(".js-priceplusmainte").text()+"$");
+ //    doc.text(175,160, $(".js-depretaicioninfo").text()+"$");
   
     
-    doc.text(175,190, $(".js-timenow").text());
+ //    doc.text(175,190, $(".js-timenow").text());
 
-	  doc.setTextColor(255, 0, 0);
-        doc.text(55,110, price+" $");
-        doc.text(175,170, $(".js-profit").text()+"$");
-        doc.text(10,170, "PROFIT: ");
+	//   doc.setTextColor(255, 0, 0);
+ //        doc.text(55,110, price+" $");
+ //        doc.text(175,170, $(".js-profit").text()+"$");
+ //        doc.text(10,170, "PROFIT: ");
         
 
-     doc.setTextColor(255, 122, 0)
-        doc.text(10,180, "RUNNING TIME: ");
-        doc.text(165,180, $(".js-runningtime").text()+" years");
-        doc.text(185,180, $(".js-runningtimedays").text()+"days");
-     doc.setTextColor(255, 255, 255);
-     doc.setFontSize(30);
-        doc.text(58,20,"payEquipment.com");
+ //     doc.setTextColor(255, 122, 0)
+ //        doc.text(10,180, "RUNNING TIME: ");
+ //        doc.text(165,180, $(".js-runningtime").text()+" years");
+ //        doc.text(185,180, $(".js-runningtimedays").text()+"days");
+ //     doc.setTextColor(255, 255, 255);
+ //     doc.setFontSize(30);
+ //        doc.text(58,20,"payEquipment.com");
      
 
- doc.save(one_equipment.name +"_"+ one_equipment.serial);
+ //doc.save(one_equipment.name +"_"+ one_equipment.serial);
+    doc.save("_testing");
 };
 
 
